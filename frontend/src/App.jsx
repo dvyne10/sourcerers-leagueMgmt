83ee531784972c4d3b6282dd442eeecfd5ddb902
bysnakejnr <barisberber.ca@gmail.com>
2023-06-07 09:09:00 -0400

    backend connected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,43 @@
-import './App.css'
-import { Button } from 'react-bootstrap';
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Button from 'react-bootstrap/Button';
+import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
-function App() {
 
+export default function App() {
+  const [message, setMessage] = useState("");
+
+
+  const myFunc= () =>{axios.get("http://localhost:8000/leagues")
+  .then((res) => {
+    setMessage(res.data.message);
+  });}
   return (
     <>
-      <Button variant="primary">Primary</Button>
+    <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+      <h1>{message}</h1>
+      <Button variant="primary" onClick={()=>myFunc()}>Primary</Button>
+    </div>
     </>
-  )
+  );
 }
-
-export default App
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
