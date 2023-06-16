58efcedbb168a5f2397ea8bf53d5c3cee3ee9dda
dvyne10 <dagadu.divine@gmail.com>
2023-06-15 22:25:40 -0400

    chore: project setup

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ using yarn for package management for the frontend of the project
 # frontend project setup
 - `cd frontend && yarn` to install all packages
 - `yarn dev` to run the project using vite.
+
+# frontend project structure
+
+# backend project structure
+- the routes folder for all the basic routing of the app
+- controller to manage the logic of the routes
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const path = require("path");
+import express from "express";
+import path from "path";
+import cors from "cors";
+import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
+
 const app = express();
-const cors = require("cors");
 const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
 
+/**
+ * - POST /api/users - Register a user
+ * - POST /api/users/auth - Authenticate a user and get a token
+ * - POST /api/users/logout - logout user and clear cookie
+ * - GET /api/users/profile - Get user profile
+ * - PUT /api/users/profile - update user profile
+ */
 
-app.get('/leagues', (req,res)=>{
-
-    res.send({message: "Welcome to the Leagues page."})
+app.get("/leagues", (req, res) => {
+  res.send({ message: "Welcome to the Leagues page." });
 });
 
+app.use(notFound);
+app.use(errorHandler);
 
-app.listen(port, ()=>{
-    console.log(`Server listening on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
