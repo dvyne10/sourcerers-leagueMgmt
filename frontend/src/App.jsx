a730e58c2a101117156a0aea0d4285b12c70f16f
HyunSeongLee <devhyun05@gmail.com>
2023-06-23 13:09:01 -0400

    changing main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,11 @@ import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
 import NavigationComponent from "./components/NavigationComponent";
-import FooterComponent from './components/FooterComponent';
+
 
 
 export default function App() {
+  
   
   return (
     
@@ -13,7 +14,7 @@ export default function App() {
       <div className="App">
         <NavigationComponent />
         <RouterProvider router={routes} />
-        <FooterComponent />        
+        {/* <FooterComponent />         */}
       </div>
     </>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,61 @@
-
-
+import Card from 'react-bootstrap/Card'; 
+import Button from 'react-bootstrap/Button'; 
+import '../App.css';
 const Home = () => {
   return (
     <>
     <div className="App" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", paddingTop: "50px"}}>
-      <h6>Top 10 ongoing</h6>
+      <h6>Top 4 ongoing</h6>
       <h1>LEAGUES</h1>
-      <div style={{ border: "1px solid black", width: "1500px", height: "500px" }}></div>
+      <div className="fire-animation "style={{ border: "1px solid black", width: "1200px", height: "500px", display: "flex", flexWrap: "wrap"}}>
+      <Card style={{ width: '18rem', objectFit: "contain", maxHeight: "100%"}}>
+      <Card.Img variant="top" src="/soccer_pic1.jpg" style={{width: "100%", height: "70%"}}/>
+        <Card.Body>
+          <Card.Title>Toronto Soccer Club</Card.Title>
+          <Card.Text>
+            We have super passion
+          </Card.Text>
+          <Button variant="primary">Check details</Button>
+        </Card.Body>
+       </Card>
+       <Card style={{ width: '18rem', objectFit: "contain", maxHeight: "100%"}}>
+      <Card.Img variant="top" src="/basketball_pic1.jpg" style={{width: "100%", height: "70%"}}/>
+        <Card.Body>
+          <Card.Title>Toronto BasketBall Club</Card.Title>
+          <Card.Text>
+            We have super passion
+          </Card.Text>
+          <Button variant="primary">Check details</Button>
+        </Card.Body>
+       </Card>
+       <Card style={{ width: '18rem', objectFit: "contain", maxHeight: "100%"}}>
+      <Card.Img variant="top" src="/soccer_pic2.jpg" style={{width: "100%", height: "70%"}}/>
+        <Card.Body>
+          <Card.Title>Avengers</Card.Title>
+          <Card.Text>
+            We love to play soccer
+          </Card.Text>
+          <Button variant="primary">Check details</Button>
+        </Card.Body>
+       </Card>
+       <Card style={{ width: '18rem', objectFit: "contain", maxHeight: "100%"}}>
+      <Card.Img variant="top" src="/basketball_pic2.jpg" style={{width: "100%", height: "70%"}}/>
+        <Card.Body>
+          <Card.Title>Jordan spirit</Card.Title>
+          <Card.Text>
+            We love to play basketball
+          </Card.Text>
+          <Button variant="primary">Check details</Button>
+        </Card.Body>
+       </Card>
+    
+       
+
+      </div>
       
+      <br/><br/>
+      <h1>ANNOUNCEMENTS</h1>
+      <div style={{border: "1px solid black", width: "1200px", height: "500px"}}></div>
     </div>
     </>
   );
