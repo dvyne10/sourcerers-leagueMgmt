220fbf146836d3d632e1dad694df4d8bdefb40be
janiceleehur <janicelee0908@gmail.com>
2023-06-26 01:33:50 -0400

    aboutus

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,68 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 const About = () => {
-    return (
-        <>
-        This is About Us Page!
-        </>
-    )
+
+  return (
+    <>
+  <div className="App" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", paddingTop: "7rem", paddingBottom: "50px"}}>
+    <h1 style={{align: "center"}}>ABOUT US</h1>
+    </div>  
+    <div className="container  bg-light d-flex justify-content-center" 
+    style={{ alignItems: "center",justifyContent: "center", paddingTop: "5rem", paddingLeft: "10rem"}}>
+ <Card style={{ width: '20rem'}}>
+        <Card.Img variant="top" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+          </Card.Text>
+          <Button variant="primary">URL</Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '20rem' }}>
+        <Card.Img variant="top" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+          </Card.Text>
+          <Button variant="primary">URL</Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '20rem' }}>
+        <Card.Img variant="top" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+          </Card.Text>
+          <Button variant="primary">URL</Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '20rem' }}>
+        <Card.Img variant="top" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+          </Card.Text>
+          <Button variant="primary">URL</Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '20rem' }}>
+        <Card.Img variant="top" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+          </Card.Text>
+          <Button variant="primary">URL</Button>
+        </Card.Body>
+      </Card>
+      </div>
+      </>
+  )
 }
 
 export default About; 
