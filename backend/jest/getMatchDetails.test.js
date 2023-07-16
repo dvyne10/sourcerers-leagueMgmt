8c6b055718fbc68487a1f547b923318c4eee796e
HyunSeongLee <devhyun05@gmail.com>
2023-07-16 11:35:48 -0400

    getMatchDetails automated testing done with mockup data

--- backend/jest/getMatchDetails.test.js
+++ backend/jest/getMatchDetails.test.js
@@ -14,20 +14,51 @@
  *
  */
 
-function getMatchDetails(matchId) {
- 
+function getMatchDetails(matchId, statusToRetrieve, matchCollection) {
     let matchObject = {requestStatus: '', errMsg: ''}; 
 
     if (!matchId || matchId.trim() === '') {
         matchObject.requestStatus = 'RJCT'; 
         matchObject.errMsg = "Match is invalid.";
+    } else if (statusToRetrieve !== 'ACTV' && statusToRetrieve !== 'PEND') {
+        matchObject.requestStatus = 'RJCT';
+        matchObject.errMsg = "Match status to retrieve is invalid."; 
+    } else {
+        let match = matchCollection.find(
+            (match) => 
+                match.matchId === matchId && match.status === statusToRetrieve
+        );
+
+        if (!match) {
+            matchObject.requestStatus = 'RJCT';
+            matchObject.errMsg = "Match is not found.";
+        } else {
+            matchObject.requestStatus = 'ACTC'
+        }
     }
 
     return matchObject; 
 }
 
 describe('retrieve match function', () => {
-    
+    matchId = '12345' // temp 
+    // Mock Matches Collection 
+    let matchesCollection = [
+        {
+        matchId: '123',
+        status: 'ACTV',
+        dateOfMatch: '2023-06-18T09:00:00.000+00:00',
+        locationOfMatch: 'Cheery Beach Sports Fields',
+        },
+        {
+        matchId: '',
+        status: 'PEND',
+        dateOfMatch: '2023-07-18T09:00:00.000+00:00',
+        locationOfMatch: 'Sunny Beach Sports Fields',
+        },
+
+    ];
+    // matchId empty or contains blank/s only 
     test('should reject if match id is null', () => {
         let result = getMatchDetails(null);
         console.log(result);
@@ -53,7 +84,43 @@ describe('retrieve match function', () => {
         expect(result.errMsg).toBe('Match is invalid.');
     });
 
+    // statusToRetrieve != 'ACTV' and != 'PEND'
+    test('should reject if statusToRetrieve is not equal to ACTV or PEND ', () => {
+        let result = getMatchDetails(matchId, 'ACTC'); 
+        expect(result.requestStatus).toBe('RJCT');
+        expect(result.errMsg).toBe('Match status to retrieve is invalid.')
+    });
 
+    test('should reject if statusToRetrieve is null', () => {
+        let result = getMatchDetails(matchId, null);
+        expect(result.requestStatus).toBe('RJCT');
+        expect(result.errMsg).toBe('Match status to retrieve is invalid.');
+    });
+    
+    test('should reject if statusToRetrieve is undefined', () => {
+        let result = getMatchDetails(matchId, undefined);
+        expect(result.requestStatus).toBe('RJCT');
+        expect(result.errMsg).toBe('Match status to retrieve is invalid.');
+    });
+    
+    test('should reject if statusToRetrieve is an empty string', () => {
+        let result = getMatchDetails(matchId, '');
+        expect(result.requestStatus).toBe('RJCT');
+        expect(result.errMsg).toBe('Match status to retrieve is invalid.');
+    });
+
+    // matchId + statusToRetrieve is not found in matches collection
+    test('should reject if matchId, statusToRetrieve is not found in matches collection', () => {
+        let result = getMatchDetails('1236', 'ACTV', matchesCollection); 
+        expect(result.requestStatus).toBe('RJCT');
+        expect(result.errMsg).toBe('Match is not found.');
+    });
+
+    // matchId + statusToRetrieve found in matches collection
+    test('found item from the match collection', () => {
+        let result = getMatchDetails('123', 'ACTV', matchesCollection); 
+        expect(result.requestStatus).toBe('ACTC'); 
+    }); 
 });
   
 
