fa597a475ceb693ecf2c3c5d263b5337c9432c96
dvyne10 <dagadu.divine@gmail.com>
2023-08-01 12:18:05 -0400

    chore: config for auth middleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-import jwt, { decode } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
 export const authenticate = async (req, res, next) => {
@@ -6,7 +6,14 @@ export const authenticate = async (req, res, next) => {
   token = req.cookies.jwt;
 
   if (token) {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+      if (err) {
+        res.status(404).send({ message: "Invalid token" });
+      }
+
+      return data;
+    });
+
     const user = await User.findById(decoded.userId);
 
     req.user = user;
@@ -18,4 +25,24 @@ export const authenticate = async (req, res, next) => {
   } else {
     res.status(404).send({ message: "not Authorized" });
   }
+};
+
+export const getTokenFromCookies = async (req, res, next) => {
+  let token;
+  let userId;
+  if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+      if (err) {
+        req.userId = null;
+      }
+
+      return data;
+    });
+
+    userId = decoded.userId;
+  }
+
+  req.userId = userId;
+  next();
 };
