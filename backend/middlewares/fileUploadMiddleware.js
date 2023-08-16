45e102a86f126c6ae561ee15f18b2242479402b7
dvyne10 <dagadu.divine@gmail.com>
2023-08-16 12:57:52 -0400

    fix: upload fix

--- backend/middlewares/fileUploadMiddleware.js
+++ backend/middlewares/fileUploadMiddleware.js
@@ -1,14 +1,18 @@
 import path from "path";
 import multer from "multer";
 
+const photoUploadDir = path.resolve("images", "profilepictures/");
+
 const storageProfile = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.resolve("images", "profilepictures/"));
   },
   filename: function (req, file, cb) {
-    console.log("23 " + req.user._id.toString())
+    console.log("23 " + req.user._id.toString());
     cb(null, `${req.user._id.toString()}.jpeg`);
   },
 });
 
-export const updateProfilePic = multer({ storage: storageProfile }).single("image");
+export const updateProfilePic = multer({ storage: storageProfile }).single(
+  "image"
+);
